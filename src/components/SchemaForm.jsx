ad5de719b6e02b6df3cd7a695e27a93b81966d00
NiLAy <nilaydhandhukiya06@gmail.com>
2024-10-09 15:29:23 +0530

    example schemaAdd

--- src/components/SchemaForm.jsx
+++ src/components/SchemaForm.jsx
@@ -43,13 +43,124 @@ const SchemaForm = () => {
     }
   };
 
+  const exampleSchemas = {
+    default: [{ name: "", type: "String" }],
+    student: [
+      { name: "name", type: "String" },
+      { name: "rollNo", type: "Number" },
+      { name: "cpi", type: "String" },
+    ],
+    product: [
+      { name: "productName", type: "String" },
+      { name: "price", type: "Number" },
+      { name: "inStock", type: "Boolean" },
+    ],
+    event: [
+      { name: "eventName", type: "String" },
+      { name: "date", type: "Date" },
+      { name: "attendees", type: "Number" },
+    ],
+    tasks: [
+      {
+        name: "taskName",
+        type: "String",
+      },
+      {
+        name: "isCompleted",
+        type: "Boolean",
+      },
+      {
+        name: "dueDate",
+        type: "Date",
+      },
+    ],
+  };
+
+  const handleExampleSelect = (e) => {
+    const selectedExample = e.target.value;
+    setFields(exampleSchemas[selectedExample]);
+  };
+
   return (
-    <div className="min-h-screen bg-white bg-gray-100 flex flex-col items-center justify-center p-4">
+    <div className="min-h-screen bg-white bg-gray-100 flex space-y-5 flex-col items-center justify-center p-4">
+      <h1 className="text-3xl font-bold mb-8 text-black">
+        Welcome to the Ultimate CRUDing ! <br />
+      </h1>
+      <h3>
+        {" "}
+        <b>
+          <i>
+            <span className="bg-[#dee2e6] ">E</span>verything
+          </i>{" "}
+          e<span className="bg-[#dee2e6] ">V</span>eryw
+          <span className="bg-[#dee2e6] ">H</span>ere{" "}
+          <i>
+            a<span className="bg-[#dee2e6] ">L</span>l
+          </i>{" "}
+          <span className="bg-[#dee2e6] ">A</span>t o
+          <span className="bg-[#dee2e6] ">N</span>ce
+        </b>
+      </h3>
+      <p>
+        Are you tired of being limited by boring, run-of-the-mill CRUD apps that
+        only do one thing? Do you dream of a world where you can create a{" "}
+        <b className="bg-[#dee2e6]">to-do list</b> , manage your{" "}
+        <b className="bg-[#dee2e6] ">e-commerce cart</b>, track your{" "}
+        <b className="bg-[#dee2e6] ">expenses</b>, and even organize your cat's
+        birthday party <i>all in one place</i>?
+      </p>
+      <ul>
+        <li>
+          <b>Flexibility :</b> Create any schema you want, from simple to
+          complex.
+        </li>
+        <li>
+          <b>Power :</b> Built on the MEAN stack, ensuring robust performance
+          and scalability.
+        </li>
+        <li>
+          <b>Ease of Use :</b> User-friendly interface makes data management a
+          breeze.
+        </li>
+        <li>
+          {" "}
+          <b>Fun :</b> Because who said CRUD operations can't be entertaining?
+        </li>
+      </ul>
       <h2 className="text-3xl font-bold mb-8 text-black">
-        Create MongoDB Schema
+        You got it! Want to organize your cat's birthday party? Um,
       </h2>
+      <div className="space-y-3">
+        <label htmlFor="exampleSelect">
+          <h2>
+            {" "}
+            <b>Lazy as me?</b>... Choose from readyMate schema:{" "}
+          </h2>
+        </label>
+        <select
+          className="w-50 p-2 border rounded-md bg-[#dee2e6]"
+          id="exampleSelect"
+          onChange={handleExampleSelect}
+        >
+          <option className="bg-white text-black" value="default">
+            Custom
+          </option>
+          <option className="bg-white text-black" value="tasks">
+            Task
+          </option>
+          <option className="bg-white text-black" value="student">
+            Student
+          </option>
+          <option className="bg-white text-black" value="product">
+            Product
+          </option>
+          <option className="bg-white text-black" value="event">
+            Event
+          </option>
+        </select>
+      </div>
       <div className="w-full bg-white max-w-2xl bg-gray-100 p-8 rounded-lg">
-        <form className="flex-col space-y-6" onSubmit={handleSubmit}>
+        <form className="flex-col space-y-5" onSubmit={handleSubmit}>
           {fields.map((field, index) => (
             <div className="flex space-x-4" key={index}>
               <Input
@@ -76,7 +187,7 @@ const SchemaForm = () => {
           ))}
           <Button
             type="button"
-            className="w-40 bg-black hover:bg-gray-800 text-white"
+            className="w-40 bg-[#343a40] hover:bg-gray-800 text-white"
             onClick={addField}
           >
             <PlusCircle
